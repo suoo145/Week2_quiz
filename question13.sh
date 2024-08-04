25fd6c67d6ec8da7d9a29ab5d8dca51ae0a14b3c
Veronica00 <suoo145@163.com>
2024-08-04 17:16:41 +0930

    change q13

--- question13.sh
+++ question13.sh
@@ -1,8 +1,7 @@
-git branch -b new_branch
+git checkout -b new_branch
 rm *.sh
+git add .
 touch file13.txt
-
 git add file13.txt
 git commit -m "add file13.txt"
-git checkout file13.txt
 git push origin new-branch
